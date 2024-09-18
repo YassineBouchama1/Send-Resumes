ec4b2f9b4d9947c5b0e69dc0332486e9c6241c52
siskoweb <sisko4dev@gmail.com>
2024-09-18 10:55:30 +0100

    Feature : create endpoint resume

---
+++ src/app/api/resumes/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import { Resume } from '@/types'
+import fs from 'fs'
+import path from 'path'
+
+export async function GET() {
+    const resumesDir = path.join(process.cwd(), 'data', 'resumes')
+    const resumeFiles = fs.readdirSync(resumesDir)
+
+    const resumes: Resume[] = resumeFiles.map(file => {
+        const id = path.parse(file).name
+        return { id, name: `Resume ${id}`, filename: file }
+    })
+
+    return NextResponse.json(resumes)
+}
